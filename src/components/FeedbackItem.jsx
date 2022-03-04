a98939ba7e3e0f42280cef5d890940489e7de208
Tenzin Dekyong <dhontenzin@gmail.com>
2022-03-04 22:26:09 +0100

    delete handler

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,22 @@
-function FeedbackItem({ item }) {
+import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
+
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      {/* passing item.id upward to FeedbackList(parent) */}
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
