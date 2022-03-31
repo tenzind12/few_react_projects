5e6cb0f22c825fe1ebf303a9c1b93ebd62f8c648
Tenzin Dekyong <82953271+tenzind12@users.noreply.github.com>
2022-03-31 09:59:04 +0200

    Update App.js

--- App.js
+++ App.js
@@ -19,10 +19,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
                               
